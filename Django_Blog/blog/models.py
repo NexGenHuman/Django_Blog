dfef77fc3a0f74bb81faa6a2749661925a5dcf0c
szymongasiewski <szymongasiewski999@gmail.com>
2023-04-25 23:26:50 +0200

    Added .gitignore Added requirements.txt Some models suggestions

--- Django_Blog/blog/models.py
+++ Django_Blog/blog/models.py
@@ -27,20 +27,29 @@ class Post(models.Model):
     likes = models.ManyToManyField(User, blank=True, related_name='post_likes')
     requires_permission = models.BooleanField(default=False)
     password = models.CharField(max_length=50, null=True, blank=True)
-    category = models.CharField(max_length=50)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+
+    class Meta:
+        ordering = ['-publication_date']    # default ordering for posts
     
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE) # --------------------- Should the comment be deleted if the user is deleted?
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    text = models.TextField()
+    text = models.TextField(max_length=250)
     publication_date = models.DateTimeField(auto_now_add=True)
     
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # --------------------- Any more ideas for profile fields?
-    description = models.TextField()
+    location = models.CharField(max_length=50, null=True, blank=True)   # ???
+    facebook = models.URLField(null=True, blank=True)                   # ???
+    twitter = models.URLField(null=True, blank=True)                    # ???
+    instagram = models.URLField(null=True, blank=True)                  # ???
+    tiktok = models.URLField(null=True, blank=True)                     # ???
+    description = models.TextField(max_length=250)
     profile_image = models.ImageField(null=True, blank=True, upload_to='profile_images/')
 
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(unique=True)    # ???
     
