a39140666be5787d6734eefa5cf4380a39d57824
Konrad <konradwasilewski@vp.pl>
2023-04-25 17:30:12 +0200

    Some comments related to models

--- Django_Blog/blog/models.py
+++ Django_Blog/blog/models.py
@@ -2,13 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
 
+# Model naming convention:
+# 1) Model names should be singular
+# 2) Model names should be in PascalCase
+# 3) Model names should be descriptive
+
+# Field naming convention:
+# 1) Field names should be in snake_case
+# 2) Field names should be descriptive
+
+# --------------------- We should think about CharField lengths
 class Image(models.Model):
+    # --------------------- Should we add an author field?
     title = models.CharField(max_length=50, unique=True)
     image = models.ImageField(upload_to='images/')
     publication_date = models.DateTimeField(auto_now_add=True)
 
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # --------------------- Should the post be deleted if the user is deleted?
     title = models.CharField(max_length=50)
     body = RichTextField()
     header_image = models.ImageField(null=True, blank=True, upload_to='headers/')
@@ -19,13 +30,14 @@ class Post(models.Model):
     category = models.CharField(max_length=50)
     
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE) # Just remembered to add it :-P
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # --------------------- Should the comment be deleted if the user is deleted?
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     text = models.TextField()
     publication_date = models.DateTimeField(auto_now_add=True)
     
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # --------------------- Any more ideas for profile fields?
     description = models.TextField()
     profile_image = models.ImageField(null=True, blank=True, upload_to='profile_images/')
 
