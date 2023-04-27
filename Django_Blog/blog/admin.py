0e5ecd2819563c3f3b641cf68f2d2dd82aa13a3e
szymongasiewski <szymongasiewski999@gmail.com>
2023-04-27 21:56:12 +0200

    Modified settings.py by adding 'blog' to INSTALLED_APPS Modified .gitignore Modified requirements.txt Registered models in admin.py Added templates directory with base.html and home.html Added blog/urls.py with home path Modified Django_blog/urls.py Modified views.py by adding home function

--- Django_Blog/blog/admin.py
+++ Django_Blog/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post, Comment, Category, Image, Profile
 
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Category)
+admin.site.register(Image)
+admin.site.register(Profile)
